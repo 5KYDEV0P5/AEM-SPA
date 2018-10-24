b86b3a0152a49e44beb6ce462ae5242df024eba7
SKYDEVOPS <syebbare@skydevops.in>
2018-10-24 16:31:00 +0530

    frontend-maven plugin added and react app configured to work along with maven using POM

---
+++ react-app/clientlib.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+ 
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/sdspa/clientlibs",
+ 
+    libs: {
+        name: "react-app",
+        allowProxy: true,
+        categories: ["sdspa.react"],
+        serializationFormat: "xml",
+        jsProcessor: ["min:gcc"],
+        assets: {
+            js: [
+                "build/static/**/*.js"
+            ],
+            css: [
+                "build/static/**/*.css"
+            ]
+        }
+    }
+};
